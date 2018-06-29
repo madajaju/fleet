6ebc69ba1d9f324c176bb658f6bd01eaf79c0046
madajaju <darren@pulsipher.org>
2018-06-29 13:43:14 +0800

    Fixes to removing nodes.

--- api/controllers/app/remove.js
+++ api/controllers/app/remove.js
@@ -6,15 +6,13 @@ module.exports = {
   description: ' Add description ',
 
   inputs: {
-    /* <parameter name>: {
-      description: 'The ID of the user to look up.',
-      type: '<parameter type>',
+    name: {
+      description: 'Name of the Application to remove',
+      type: 'string',
       required: true
     },
-    */
-    
     mode: {
-      description: "results format: json or html",
+      description: 'results format: json or html',
       type: 'string',
       required: false
     }
@@ -23,7 +21,7 @@ module.exports = {
   exits: {
     success: {
       responseType: 'view',
-      viewTemplatePath: 'welcome'
+      viewTemplatePath: 'dc/graph'
     },
     json: {
       responseType: '', // with return json
@@ -36,22 +34,18 @@ module.exports = {
 
   fn: async function (inputs, exits, env) {
 
-    // Look up the user whose ID was specified in the request.
-    // Note that we don't have to validate that `userId` is a number;
-    // the machine runner does this for us and returns `badRequest`
-    // if validation fails.
     try {
-      let user = await User.findOne(inputs.userId);
-      if (!user) {return exits.notFound('/signup');}
-
+      let app = await Application.findOne({name:inputs.name});
+      if (!app) {return exits.notFound('/dc/graph');}
+      await Application.destroy({name:inputs.name});
       // Display the results
-      if(inputs.mode === "json") {
+      if(inputs.mode === 'json') {
         // Return json
-        return exits.json({name: user.name});
+        return exits.json({name: inputs.name});
       }
       else {
         // Display the welcome view.
-        return exits.success({name: user.name});
+        return exits.success({name: inputs.name});
       }
     }
     catch (e) {
